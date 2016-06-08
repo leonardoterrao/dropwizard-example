303658a97ceecb041a8cd42cf383797614dab313
Leonardo Terrão <leonardo.terrao@tecsinapse.com.br>
2016-06-07 22:51:10 -0300

    views mustache and freemarker

---
+++ src/main/java/br/com/leonardoterrao/view/PersonView.java
@@ -0,0 +1,32 @@
+package br.com.leonardoterrao.view;
+
+import br.com.leonardoterrao.model.Person;
+import io.dropwizard.views.View;
+import lombok.Getter;
+
+@Getter
+public class PersonView extends View {
+
+    private final Person person;
+
+    public PersonView(Template template, Person person) {
+        super(template.getTemplateName());
+        this.person = person;
+    }
+
+    public enum Template {
+        FREEMARKER("freemarker/person.ftl"),
+        MUSTACHE("mustache/person.mustache");
+
+        private String templateName;
+
+        Template(String templateName) {
+            this.templateName = templateName;
+        }
+
+        public String getTemplateName() {
+            return templateName;
+        }
+    }
+
+}

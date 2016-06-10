30d75be8e65d10ca30a61e148d83299ce44d9c3e
Leonardo Terrão <leonardo.terrao@tecsinapse.com.br>
2016-06-09 23:50:55 -0300

    HelloWorldResourceTest

---
+++ src/test/java/br/com/leonardoterrao/resources/HelloWorldResourceTest.java
@@ -0,0 +1,35 @@
+package br.com.leonardoterrao.resources;
+
+import br.com.leonardoterrao.api.Saying;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.assertj.core.api.Assertions;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.ws.rs.core.GenericType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HelloWorldResourceTest {
+
+    @ClassRule
+    public static final ResourceTestRule resouces = ResourceTestRule.builder()
+            .addResource(new HelloWorldResource("Hello, %s!", "Fulano"))
+            .build();
+
+    @Test
+    public void sayHello() {
+        final Saying response = resouces.client().target("/hello-world").request().get(new GenericType<Saying>() {});
+        assertThat(response.getContent()).isEqualTo("Hello, Fulano!");
+    }
+
+    @Test
+    public void sayHelloWithParam() {
+        final Saying response = resouces.client().target("/hello-world?name=Leonardo").request().get(new GenericType<Saying>() {});
+        assertThat(response.getContent()).isEqualTo("Hello, Leonardo!");
+    }
+
+}
